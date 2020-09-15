6cd17ada678c8c607d68c9b79ccaa086b9a4f2c1
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-09-15 17:47:14 +0100

    Add double tap to exit

--- app/src/main/java/com/tikiton/quiz/MainActivity.kt
+++ app/src/main/java/com/tikiton/quiz/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.tikiton.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        submitButton.setOnClickListener {
+            val nameFieldValue = nameField.text.toString()
+            if(nameFieldValue.length > 2) {
+                val playgroundIntent = Intent(baseContext, Playground::class.java);
+                startActivity(playgroundIntent)
+                finish()
+            } else Toast.makeText(this, "Votre nom doit avoir au moins 2 caratères", Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/tikiton/quiz/Playground.kt
+++ app/src/main/java/com/tikiton/quiz/Playground.kt
@@ -2,10 +2,30 @@ package com.tikiton.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class Playground : AppCompatActivity() {
+    private var backPressedTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playground)
+    }
+
+    // Handle back button pressed
+    override fun onBackPressed() {
+        if((backPressedTime + 2000) > System.currentTimeMillis()) {
+            // Can exit if there is 2 seconds between the back button double tap
+            super.onBackPressed()
+        } else {
+            // Neither toast call to action message
+            Toast.makeText(
+                applicationContext,
+                "Appuiyez à nouveau pour quitter",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        // Update user back pressed time
+        backPressedTime = System.currentTimeMillis()
     }
 }
